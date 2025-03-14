5c4e07a322e86fc170098c1f04c360dbb9f0274f
fran-imn <fmnarbona@uc.cl>
2025-03-14 15:34:28 -0300

    clase 14 de marzo 2025

--- codigo/ayudantia_1_clase.R
+++ codigo/ayudantia_1_clase.R
@@ -26,15 +26,36 @@ nombres <- c("fran", "trini", "jorge")
 #un dataframe
 df <- data.frame(ID = 1:3, nombres = c("fran", "trini", "jorge"), edad = c(25, 22, 23))
 
-install.packages("readr")
+#instalar paquetes
+install.packages("readr") #esto solo se hace una vez!
 install.packages("tidyverse")
-library(readr)
+library(readr) #library() tienen que correrlo cada vez que abran un proyecto
 library(tidyverse)
 
 df_forestal <- read_csv("data/01_datos_forestales.csv")
+df_forestal_limpio <- read_csv("data/01_datos_forestales_limpio_v2.csv")
+
+# Estadísticas Descriptivas
+mean(df_forestal$Altura_m, na.rm = TRUE)  # Media de la altura
+median(df_forestal$Altura_m, na.rm = TRUE) # Mediana de la altura
+sd(df_forestal$Altura_m, na.rm = TRUE)  # Desviación estándar altura
 
 head(df_forestal)
 glimpse(df_forestal)
 summary(df_forestal)
 
+unique(df_forestal$Especie)
 
+unique(df_forestal_limpio$Especie)
+
+#boxplot
+ggplot(df_forestal_limpio, aes(x = Especie, y = Altura_m, fill = Especie)) +
+         geom_boxplot()
+
+df_especies_interes <- df_forestal_limpio %>% 
+  filter(Especie %in% c("Nothofagus dombeyi", "Nothofagus pumilio"))
+
+unique(df_especies_interes$Especie)
+
+prueba_t <- t.test(Altura_m ~ Especie, data = df_especies_interes, var.equal = TRUE)
+print(prueba_t)
